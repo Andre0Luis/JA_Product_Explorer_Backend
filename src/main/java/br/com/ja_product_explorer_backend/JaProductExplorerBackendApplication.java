060c83f8a227067f9a9e984bdb35e2b681a3ce1a
Andre0Luis <aluis283@gmail.com>
2024-02-18 16:48:00 -0300

    base_project

---
+++ src/main/java/br/com/ja_product_explorer_backend/JaProductExplorerBackendApplication.java
@@ -0,0 +1,13 @@
+package br.com.ja_product_explorer_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JaProductExplorerBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JaProductExplorerBackendApplication.class, args);
+	}
+
+}
