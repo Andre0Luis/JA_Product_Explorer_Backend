98a968f19b0c97ba2895d8bb0ba3632fe72d89a6
Andre0Luis <aluis283@gmail.com>
2024-02-18 20:24:54 -0300

    first call

--- src/main/java/br/com/ja_product_explorer_backend/resource/SearchExplorerResource.java
+++ src/main/java/br/com/ja_product_explorer_backend/resource/SearchExplorerResource.java
@@ -1,4 +1,30 @@
 package br.com.ja_product_explorer_backend.resource;
 
+import br.com.ja_product_explorer_backend.model.Product;
+import br.com.ja_product_explorer_backend.service.impl.SearchExplorerServiceImpl;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+@RequestMapping("product/")
 public class SearchExplorerResource {
+
+    private SearchExplorerServiceImpl explorerService;
+
+    @Autowired
+    public SearchExplorerResource(SearchExplorerServiceImpl explorerService) {
+        this.explorerService = explorerService;
+    }
+
+    @GetMapping("{code}")
+    public ResponseEntity<Product> findProduct(@PathVariable String code){
+   
+        return ResponseEntity.ok().body(explorerService.getProductByCodeBar(code));
+    }
+
 }
--- src/main/java/br/com/ja_product_explorer_backend/service/impl/SearchExplorerServiceImpl.java
+++ src/main/java/br/com/ja_product_explorer_backend/service/impl/SearchExplorerServiceImpl.java
@@ -1,4 +1,23 @@
 package br.com.ja_product_explorer_backend.service.impl;
 
+import br.com.ja_product_explorer_backend.model.Product;
+import br.com.ja_product_explorer_backend.repository.ProductRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
 public class SearchExplorerServiceImpl {
+
+    private final ProductRepository productRepository;
+
+    @Autowired
+    public SearchExplorerServiceImpl(ProductRepository productRepository) {
+        this.productRepository = productRepository;
+    }
+
+    public Product getProductByCodeBar(String code){
+
+        return productRepository.findByIdCodigoDeBarras(code);
+
+    }
 }
