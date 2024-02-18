142afa675a6255d68a6c792fd11a99c68112f019
Andre0Luis <aluis283@gmail.com>
2024-02-18 20:05:50 -0300

    add conection

--- src/main/java/br/com/ja_product_explorer_backend/repository/ProductRepository.java
+++ src/main/java/br/com/ja_product_explorer_backend/repository/ProductRepository.java
@@ -1,4 +1,9 @@
 package br.com.ja_product_explorer_backend.repository;
 
-public interface ProductRepository {
+import br.com.ja_product_explorer_backend.model.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ProductRepository extends JpaRepository<Product, Long> {
 }
