f8c788ca9ee6c96d2a9fa599ec1d24e186ef2a84
Andre0Luis <aluis283@gmail.com>
2024-06-23 13:29:30 -0300

    test

---
+++ src/main/java/br/com/ja_product_explorer_backend/resource/InventoryResource.java
@@ -0,0 +1,11 @@
+package br.com.ja_product_explorer_backend.resource;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("inventory/")
+public class InventoryResource {
+
+
+}
