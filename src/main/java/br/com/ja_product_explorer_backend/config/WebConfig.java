b556e01f3f908c0d021236ba1a4bbfaf13bdf644
Andre0Luis <aluis283@gmail.com>
2024-08-22 16:48:30 -0300

    edit cors

--- src/main/java/br/com/ja_product_explorer_backend/config/WebConfig.java
+++ src/main/java/br/com/ja_product_explorer_backend/config/WebConfig.java
@@ -10,7 +10,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:3000") // Substitua pela URL do seu frontend
+                .allowedOrigins("http://localhost:5173", "https://ja-product-explorer-frontend-16b7d68216f1.herokuapp.com/")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true);
