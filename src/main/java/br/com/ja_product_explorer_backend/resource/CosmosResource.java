0342b7779b72a158c75d50723047365c501d3f73
Andre0Luis <aluis283@gmail.com>
2024-08-22 16:50:56 -0300

    edit address endpoints

--- src/main/java/br/com/ja_product_explorer_backend/resource/CosmosResource.java
+++ src/main/java/br/com/ja_product_explorer_backend/resource/CosmosResource.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import reactor.core.publisher.Flux;
 
 @Controller
-@RequestMapping("/cosmos")
+@RequestMapping("/api/cosmos")
 public class CosmosResource {
 
     private final CosmosService cosmosService;
--- src/main/java/br/com/ja_product_explorer_backend/resource/SearchExplorerResource.java
+++ src/main/java/br/com/ja_product_explorer_backend/resource/SearchExplorerResource.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-@RequestMapping("/products")
+@RequestMapping("/api/products")
 public class SearchExplorerResource {
 
     private final SearchExplorerService explorerService;
